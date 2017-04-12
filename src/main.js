9ccb443d5dde348423a3ccaf9506d9066069dd26
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2017-04-12 18:51:06 +0300

    Implement panning and zooming

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-console.log("Hello BiSet");
+let svg = d3
+  .select("svg")
+  .call(d3
+    .zoom()
+    .scaleExtent([1, 10])
+    .on("zoom", handleZoom)
+  );
+
+let surface = svg
+  .append("g");
+
+let circles = surface
+  .selectAll("circle")
+  .data([{x: 50, y: 50}])
+  .enter()
+    .append("circle")
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y)
+    .attr("r", 20)
+    .style("fill", 'red');
+
+function handleZoom() {
+  let x = d3.event.transform.x;
+  let y = d3.event.transform.y;
+  let k = d3.event.transform.k;
+  surface.attr("transform", `translate(${x}, ${y}) scale(${k})`);
+}
